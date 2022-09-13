8ba20cd0a87be750458cfb31f2310184e37a2ae2
liyukun <liyukun@cryptape.com>
2022-09-13 10:54:38 +0800

    chore: make secp256k1 as submodule

--- contracts/c/deps/ckb-c-stdlib/libc/stdbool.h
+++
@@ -1,9 +0,0 @@
-#ifndef CKB_C_STDLIB_STDBOOL_H_
-#define CKB_C_STDLIB_STDBOOL_H_
-#include <entry.h>
-
-#define true 1
-#define false 0
-#define bool _Bool
-
-#endif /* CKB_C_STDLIB_STDBOOL_H_ */
--- contracts/c/deps/ckb-c-stdlib/libc/stddef.h
+++
@@ -1,8 +0,0 @@
-#ifndef CKB_C_STDLIB_STDDEF_H_
-#define CKB_C_STDLIB_STDDEF_H_
-
-#include "internal/types.h"
-#define NULL ((void*)0)
-#include <entry.h>
-
-#endif /* CKB_C_STDLIB_STDDEF_H_ */
